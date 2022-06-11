6373b7d3e5422f52e3e64777e8366517c0dd50a6
Roman Geathers <romangeathersdev@gmail.com>
2022-06-11 15:19:16 -0400

    Write caesar cipher problem for practice

---
+++ caesar-cipher.rb
@@ -0,0 +1,27 @@
+def caesar_cipher(word, number = 0)
+    arr = word.split("")
+    new_word = []
+    arr.each do |char|
+        char_ascii = char.ord
+        if char_ascii.between?(65, 90)
+            char_ascii += number
+            if char_ascii > 90
+                char_ascii = char_ascii -= 26
+            end
+            new_word << char_ascii.chr
+        elsif char_ascii.between?(97, 122)
+            char_ascii += number
+            if char_ascii > 122
+                char_ascii -= 26
+            end
+            new_word << char_ascii.chr 
+        else
+            new_word << char
+        end
+    end
+    new_word = new_word.join
+    p new_word
+end
+
+# testing to see if it works
+caesar_cipher("What a string!", 5)
